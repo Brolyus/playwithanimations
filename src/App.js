310bae09177212bf2e50917bad2ea5aab3d944ee
Anthony De Sousa <anthonydesousa@MacBook-Pro-de-Anthony.local>
2021-03-08 08:52:30 +0100

    pushing test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>test</p>
+
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <div className="sprite">
+      <div className="test"></div>
+      </div>
+      <footer className="footer">
+        <ul>
+          <li>test</li>
+          <li>test</li>
+          <li>test</li>
+        </ul>
+      </footer>
     </div>
   );
 }
